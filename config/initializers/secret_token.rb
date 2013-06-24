4b187c8ef9d63ee83822b2fdcd917e284133d4d4
Sundari <sundari@hawk.(none)>
2013-06-24 11:28:18 +0530

    Emails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Email::Application.config.secret_token = 'b502b5545ef530f7d71ad51d186177bd4bfabe81da12143fc405dc260e8e8cf36e9e0ae6c2d89d516e192a0477ac9c1a7b2aa9364cfbc16728c2617804edc9d5'
